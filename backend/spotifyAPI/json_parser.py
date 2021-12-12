5fdadc54d663204387252b81f13893f639aa3c71
Oliver Preuß <o-preuss@gmx.de>
2021-12-12 18:54:50 +0100

    little library to for faster json loading

---
+++ backend/spotifyAPI/json_parser.py
@@ -0,0 +1,31 @@
+import cysimdjson
+import json
+
+def load_json(path: str) -> dict:
+    parser = cysimdjson.JSONParser()
+    json = parser.load(path)
+
+    parsed_json = _parse_cysimdjson_JSONObject(json)
+
+    return parsed_json
+
+def _parse_cysimdjson_JSONObject(JSONObject) -> dict:
+    parsed_json = {}
+    for item in JSONObject.items():
+        if type(item[1]) == cysimdjson.JSONObject:
+            # print(234)
+            parsed_json[item[0]] = _parse_cysimdjson_JSONObject(item[1])
+        elif type(item[1]) == cysimdjson.JSONArray:
+            parsed_json[item[0]] = item[1].export()
+        else:
+            # print(1234)
+            parsed_json[item[0]] = item[1]
+    
+    return parsed_json
+
+def dump_json(data: dict, path: str):
+    with open(os.path.join(path), "w") as f:
+        json.dump(data, f)
+
+def pretty_print_json(data: dict):
+    print(json.dumps(data, indent=2))
