37bf2d22ba77c04498861c3960cd597b7206aa7c
Felix Kuhn <felixkuhn@wl-pool2-pnt-09976.uni-muenster.de>
2022-01-10 18:29:48 +0100

    spotify login works in backend; not in frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,20 +1,45 @@
 import { useState } from 'react'
 import logo from './logo.svg'
 import './App.css'
-import CoordinateSystem from './compoents/CoordinateSystem'
+import CoordinateSystem from './components/CoordinateSystem'
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false)
   const [count, setCount] = useState(0)
+  const [isSpotifyAuthenticated, setIsSpotifyAuthenticated] = useState(false)
+
+  const authenticateSpotify = () => {
+    fetch('http://127.0.0.1:8000/spotify/is-authenticated')
+      .then(res => res.json())
+      .then(data => {
+        console.log("IS AUTHENTICATED Data:", data)
+        setIsSpotifyAuthenticated(data.status)
+        if(!data.status) {
+          fetch('http://127.0.0.1:8000/spotify/get-auth-url')
+            .then(res => res.json())
+            .then(data => {
+              console.log("GET AUTH URL Data:", data)
+              window.location.replace(data.url)
+            })
+        }
+      })
+  }
+
+  const test = () => {
+    console.log(isSpotifyAuthenticated)
+  }
+
 
   const handleClick = () => {
-    fetch("http://localhost:8000/users/1/")
+    fetch("http://127.0.0.1:8000/users/1/")
       .then(res => res.json())
       .then(json => console.log(json))
   }
   
   return (
     <div className='flex justify-center align-middle' style={{ backgroundColor: 'green', height: '700px'}}>
+      <button onClick={authenticateSpotify}>CLICK MICH HART</button>
+      <button onClick={test}>Test</button>
       <CoordinateSystem />
     </div>
   )
