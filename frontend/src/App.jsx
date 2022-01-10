c8b25f84951382b2e40a500f94c4862137b0fa2c
Felix Kuhn <felixkuhn@wl-pool2-pnt-09976.uni-muenster.de>
2022-01-10 12:00:08 +0100

    we ♥️  git

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,9 +2,8 @@ import { useState } from 'react'
 import logo from './logo.svg'
 import './App.css'
 
-import { Link } from "react-router-dom";
-
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false)
   const [count, setCount] = useState(0)
 
   const handleClick = () => {
@@ -13,48 +12,12 @@ function App() {
       .then(json => console.log(json))
   }
 
-
-
-  return (
-    <div className="App">
-        <Link to="/home">Invoices</Link> |{" "}
-        <Link to="/login">Expenses</Link>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <div onClick={handleClick}>
-          Call Backend
-        </div>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.jsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
-    </div>
-  )
+  if(!isLoggedIn)
+    return (
+      <div></div>
+    )
+  else
+      return
 }
 
 export default App
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,31 +3,10 @@ import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
 
-import {
-  BrowserRouter,
-  Routes,
-  Route
-} from "react-router-dom";
-import Login from './pages/Login';
-import Home from './pages/Home';
-
-
-
-// ReactDOM.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>,
-//   document.getElementById('root')
-// )
-
 
 ReactDOM.render(
-  <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<App />} />
-      <Route path="login" element={<Login />} />
-      <Route path="home" element={<Home />} />
-    </Routes>
-  </BrowserRouter>,
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
   document.getElementById('root')
 )
