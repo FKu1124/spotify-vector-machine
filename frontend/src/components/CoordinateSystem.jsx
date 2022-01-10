82eb899d940061b30948b121e84794f6fb1cfff6
Marc Gehrke <m.gehrke@business-unicorns.de>
2022-01-10 18:57:30 +0100

    [WIP] Coordinate component

--- frontend/src/components/CoordinateSystem.jsx
+++ frontend/src/components/CoordinateSystem.jsx
@@ -1,17 +1,72 @@
-import React from 'react'
+import { React, useState } from 'react'
+import moodMapping from '../static/mood.json'
+import Coordinate from './Coordinate'
 
 export default function CoordinateSystem() {
 
-	const handleMouse = (e) => {
-    console.log(`X: ${e.nativeEvent.offsetX} Y: ${e.nativeEvent.offsetY}`)
+  const square_site_length = 400
+  let moodArr = []
+
+  moodMapping.forEach(item => (
+    moodArr.push(
+      {
+        "top": getScaledCoordinate(item.coordinate.y),
+        "left": getScaledCoordinate(item.coordinate.x),
+        "label": item.label
+      }
+    )
+  ))
+
+  const moodCoordinates = moodArr
+
+  const [drawing, setDrawing] = useState(false)
+  const [coordinates, setCoordinates] = useState(moodCoordinates)
+
+  function getScaledCoordinate(val) {
+    let val_scaled = (val + 1) / 2
+    return val_scaled * square_site_length
   }
 
-  return (   
-    <div onMouseDown={e => handleMouse(e)} className='align-middle relative' style={{ width: "400px", height: "400px", backgroundColor:"red" }}>
-			<div style={{position:'absolute', top:'200px', left:'0px', height:'2px', width:'400px', backgroundColor:'grey' }} />
-			<div style={{position:'absolute', top:'0px', left:'200px', height:'400px', width:'2px', backgroundColor:'grey' }} />
-			{/* Sample Datapoint */}
-			<div style={{position:'absolute', top: '5px', left: '5px', height: '5px', width: '5px', backgroundColor: 'blue'}} />
+  const setPosition = (e, start) => {
+    let tmp_cords = [...coordinates]
+    let canvas = document.getElementById("canvas")
+    if (start) {
+      tmp_cords.push(
+        {
+          "top": e.clientY - canvas.getBoundingClientRect().top,
+          "left": e.clientX - canvas.getBoundingClientRect().left,
+          "label": "START"
+        }
+      )
+      console.log(tmp_cords);
+      setCoordinates(tmp_cords)
+    } else {
+
+    }
+  }
+
+
+  return (
+    <div
+      onMouseDown={e => setPosition(e, true)}
+      onMouseUp={e => setPosition(e, false)}
+      // onMouseMove={e => draw(e)}
+      id='canvas'
+      className='align-middle relative'
+      style={{ width: square_site_length, height: square_site_length, backgroundColor: "red", position: 'fixed' }}
+    >
+      <div style={{ position: 'absolute', top: '200px', left: '0px', height: '2px', width: square_site_length, backgroundColor: 'grey' }} />
+      <div style={{ position: 'absolute', top: '0px', left: '200px', height: square_site_length, width: '2px', backgroundColor: 'grey' }} />
+
+      {/* Mood Datapoints */}
+      {coordinates.map((item, i) => (
+        <Coordinate
+          key={i}
+          label={item.label}
+          top={item.top}
+          left={item.left}
+        />
+      ))}
     </div>
   )
 }
