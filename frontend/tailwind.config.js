27bc55687c2753c48ff964ca94a3382c0569358c
Marc Gehrke <m.gehrke@business-unicorns.de>
2022-01-10 11:54:48 +0100

    added custom colors

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,6 +2,12 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    colors: {
+      transparent: 'transparent',
+      spotify: '#1db954',
+      spotifyAlt: '#1ed760',
+      black: '#191414'
+    },
     extend: {},
   },
   variants: {
