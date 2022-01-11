924b7f2b04b0ec97c1e294baf9c03079ccc69e53
Felix Kuhn <felixkuhn@wl-pool2-pnt-09976.uni-muenster.de>
2022-01-11 11:40:26 +0100

    admin route is back

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path, include
+from django.contrib import admin
 from django.contrib.auth.models import User
 from rest_framework import serializers, viewsets, routers
 
@@ -24,5 +25,6 @@ router.register(r'users', UserViewSet)
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
     path('', include(router.urls)),
+    path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 ]
