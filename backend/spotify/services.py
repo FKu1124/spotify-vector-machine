26f481f2ea0c3381581b8c965fd716e081765afd
Oliver Preuß <o-preuss@gmx.de>
2022-01-20 13:12:36 +0100

    added scraping by artists

--- backend/spotify/services.py
+++ backend/spotify/services.py
@@ -8,7 +8,8 @@ from spotify.models import Genre, Artist, Album, Track, GenreArtist, GenreTrack
 SPOTIPY_CLIENT_ID = os.environ.get("SPOTIFY_CLIENT_ID")
 SPOTIPY_CLIENT_SECRET = os.environ.get("SPOTIFY_CLIENT_SECRET")
 
-PLAYLIST_LIMIT = 5
+PLAYLIST_LIMIT = 1
+ARTIST_LIMIT = 1
 
 spotify = spotipy.Spotify(
     client_credentials_manager=SpotifyClientCredentials(
@@ -32,7 +33,7 @@ def scrape_genres() -> None:
             pass
 
 
-def scrape_genre_top_playlists() -> None:
+def scrape_top_playlists_by_genre() -> None:
     genres = Genre.objects.all()
 
     for genre in genres:
@@ -43,13 +44,34 @@ def scrape_genre_top_playlists() -> None:
         for playlist in search_response["playlists"]["items"]:
             playlist_response = spotify.playlist(playlist["id"])
             tracks = playlist_response["tracks"]["items"]
+            tracks = [track["track"] for track in tracks]
+            save_tracks(tracks, genre)
+            break
+
+
+def scrape_top_artists_by_genre() -> None:
+    genres = Genre.objects.all()
+
+    for genre in genres:
+        search_response = spotify.search(q="genre:"+genre.name,
+                                         type="artist",
+                                         limit=PLAYLIST_LIMIT)
+
+        for artist in search_response["artists"]["items"]:
+            artist__albums_response = spotify.artist_albums(artist["id"])
+            albums = artist__albums_response["items"]
+            for album in albums:
+                album_tracks_repsonse = spotify.album_tracks(album["id"])
+                album_tracks_repsonse = album_tracks_repsonse["items"]
+                tracks = []
+                for track in album_tracks_repsonse:
+                    tracks.append(spotify.track(track["id"]))
             save_tracks(tracks, genre)
             break
 
 
 def save_tracks(tracks: List, base_genre: Genre) -> None:
     for track in tracks:
-        track = track["track"]
 
         created = False
         try:
@@ -130,33 +152,33 @@ def get_or_create_artist(artist_spotify_id: str, base_genre: Genre):
                 genre_artist_obj.base_genre = True
                 genre_artist_obj.save()
 
-        artist_obj.save()
+    artist_obj.save()
 
     return artist_obj
 
 
 def get_or_create_ablum(album_spotify_id: str, base_genre: Genre):
-    album = spotify.album(album_spotify_id)
+    album_response = spotify.album(album_spotify_id)
 
     created = False
     try:
-        album_obj = Album.objects.get(spotify_id=album["id"])
+        album_obj = Album.objects.get(spotify_id=album_response["id"])
     except:
-        album_obj = Album(spotify_id=album["id"])
+        album_obj = Album(spotify_id=album_response["id"])
         created = True
 
-    album_obj.popularity = album["popularity"]
+    album_obj.popularity = album_response["popularity"]
 
     if created:
-        album_obj.name = album["name"]
-        album_obj.spotify_id = album["id"]
-        album_obj.album_type = album["album_type"]
-        album_obj.total_tracks = album["total_tracks"]
-        album_obj.popularity = album["popularity"]
-        album_obj.release_date = album["release_date"]
+        album_obj.name = album_response["name"]
+        album_obj.spotify_id = album_response["id"]
+        album_obj.album_type = album_response["album_type"]
+        album_obj.total_tracks = album_response["total_tracks"]
+        album_obj.popularity = album_response["popularity"]
+        album_obj.release_date = album_response["release_date"]
         album_obj.save()
 
-        for artist in album["artists"]:
+        for artist in album_response["artists"]:
             artist_obj = get_or_create_artist(artist["id"], base_genre)
             album_obj.artists.add(artist_obj)
 
@@ -167,4 +189,5 @@ def get_or_create_ablum(album_spotify_id: str, base_genre: Genre):
 
 scrape_genres()
 
-scrape_genre_top_playlists()
+scrape_top_playlists_by_genre()
+scrape_top_artists_by_genre()
